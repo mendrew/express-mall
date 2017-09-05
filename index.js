b01017652a04c3a3005e100d3479570681d951d3
Andrey Medvedev <andr.medv.spb@gmail.com>
2017-09-05 19:41:24 +0300

    Get items objects with page links from categories
    Comment part of usefull links for test
    Add node-demon

--- index.js
+++ index.js
@@ -3,21 +3,38 @@ const jquery = require('jquery')
 const jsdom = require('jsdom')
 const { JSDOM } = jsdom
 const Horseman = require('node-horseman')
+const yaml = require('js-yaml');
+const fs = require('fs');
+const url = require('url');
+const parseNum = require('parse-num')
 
 const MALL_URL = 'https://mall.aliexpress.com/'
 
 parseMall()
 
-async function parseMall() {
-  const pageHtml = await getPageSourceByHorseman(
-    MALL_URL,
-    {selectorToWait: '.categories-list-box > .cl-item'}
-  )
+// testFunc()
 
-  const categories = getCategoriesFromPage(pageHtml)
-  // console.log(categories)
-  const result = await parseCategories(categories)
-  console.log(result.length)
+async function testFunc() {
+  const link = 'about:blank#'
+  const resultLink = url.resolve(MALL_URL, link)
+  console.log("Result link: ", resultLink)
+  const pageSource = await getPageSource(resultLink)
+  console.log(pageSource)
+}
+
+async function parseMall() {
+  // const pageHtml = await getPageSourceByHorseman(
+  //   MALL_URL,
+  //   {selectorToWait: '.categories-list-box > .cl-item'}
+  // )
+
+  // const categories = getCategoriesFromPage(pageHtml)
+  // fs.writeFileSync('categories.yaml', yaml.dump(categories));
+
+  const categories = yaml.safeLoad(fs.readFileSync('categories.yaml', 'utf8'));
+
+  const itemsLinks = await parseCategories(categories)
+  console.log(JSON.stringify(itemsLinks, null, 2))
 
   return true
 }
@@ -25,22 +42,6 @@ async function parseMall() {
 function getCategoryItems(category, callback) {
 }
 
-function parseCategories(categories=[]) {
-  const items = categories.map(async (category) => {
-    const subCategories = category.categories
-
-    let categoryItems = []
-
-    const subCatItems = subCategories.map(async (subCategory) => {
-
-      // add link check to prevent errors
-      const categoryPageSourse = await getPageSource(subCategory.link)
-    })
-    return await Promise.all(subCatItems)
-  })
-
-  return Promise.all(items)
-}
 
 /**
  * Get page html by Horseman
@@ -94,7 +95,11 @@ function getCategoriesFromPage(html) {
     const $subCategories = $(category).find('a')
 
     const subCategories = $.map($subCategories, (subCategory) => {
-      const categoryLink = $(subCategory).attr('href')
+      const categoryLink = getAbsoluteLink($(subCategory).get(0).href)
+      if (!categoryLink) {
+        return
+      }
+
       let categoryName = $(subCategory).text().trim()
       const groupNameMatch = categoryLink.match(/group\/(\w+)\//)
       if (!categoryName && groupNameMatch && groupNameMatch.length) {
@@ -104,12 +109,11 @@ function getCategoriesFromPage(html) {
       let category = {name: categoryName, link: categoryLink}
 
       if (!categoryName) {
-        const categoryImg = $(subCategory).find('img').attr('src')
+        const categoryimg = getabsolutelink($(subcategory).find('img').attr('src'))
         if (categoryImg) {
           category.img = categoryImg
         }
       }
-
 
       return category
     })
@@ -120,3 +124,96 @@ function getCategoriesFromPage(html) {
   return items
 }
 
+async function parseCategories(categories=[]) {
+  let categoryItems = []
+  const items = categories.map(async (category) => {
+    const subCategories = category.categories
+
+
+    const subTestCategories = [subCategories[0]]
+
+    const subCatItemsPromises = subTestCategories.map(async (subCategory) => {
+
+      const categoryPageSource = await getPageSource(subCategory.link)
+
+      const hostname = url.parse(subCategory.link).hostname;
+      const ignoreCategory = hostname.includes('sale')
+      if (ignoreCategory) {
+        return
+      }
+
+      const categoriesList = [category.name, subCategory.name]
+
+      const items = getItemsFromPage(categoryPageSource, categoriesList)
+
+      return items
+    })
+
+    const subCatItems = await Promise.all(subCatItemsPromises)
+    subCatItems.map((item) => {
+      categoryItems = categoryItems.concat(item)
+    })
+  })
+
+  const subCategoryItemsList = await Promise.all(items)
+  return categoryItems
+}
+
+function getItemsFromPage(html, categories) {
+  const document = new JSDOM(html)
+  const $ = jquery(document.window)
+
+  const $items = $('.list-items').find('.list-item')
+
+  console.log("Items: ", $items.length)
+  const items = $.map($items, (item) => {
+    const name = $(item).find('.history-item.product').attr('title')
+    const link = getAbsoluteLink(
+      $(item).find('.history-item.product').get(0).href)
+
+    if (!link) {
+      return
+    }
+
+    const priceString = $(item)
+      .find('.price .value')
+      .not('.price-before-discount,.price-del')
+      .text()
+    const price = parsePrice(priceString)
+    const img = getAbsoluteLink($(item).find('img').attr('src'))
+
+    return { name, link, price, img, categories}
+  })
+
+  return items
+}
+
+function parseSaleCatalog($) {
+  /*Sale catalog have preload content with latest items
+   * has 'sale' in hostname part of url
+   * But we can't parse easy, no classes, looks like it's
+   * content from somewhere */
+  return []
+}
+
+
+function parsePrice(priceString) {
+  const price = parseNum(priceString, ',')
+
+  if (isNaN(price)) {
+    return 0
+  }
+
+  return price
+}
+
+function getAbsoluteLink(address, from = MALL_URL) {
+  if (!address) {
+    return
+  }
+
+  const link = url.resolve(from, address)
+  if (link.startsWith('http')) {
+    return link
+  }
+}
